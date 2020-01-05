f68c94148945b8f8c51f3af9a80e6525d05d48cb
Poorab <poorab@Bhaskars-MBP-4.modem>
2020-01-05 12:20:08 +1100

    Compass padding effect

--- source/js.js
+++ source/js.js
@@ -1,16 +1,7 @@
-basic.forever(() => {
-    let degrees = input.compassHeading();
-    if (degrees < 45 || degrees > 315) {
-        basic.showString("N");
-    }
-    else if (degrees < 135) {
-        basic.showString("E");
-    }
-    else if (degrees < 225) {
-        basic.showString("S");
-    }
-    else {
-        basic.showString("W");
-    }
-});
+$(document).ready(function() {
+    $('.Compass').click(function() {
+      $(this).addClass('.Compass45') //remove class to stop animation
+    });
+  });
+
 
