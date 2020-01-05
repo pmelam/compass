ff61556bffcc97b9d29fdc2629cc0fd5c39aaf67
Poorab <poorab@Bhaskars-MBP-4.modem>
2020-01-05 14:33:22 +1100

    refactored

---
+++ source/compass.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//     $('.Compass').click(function() {
+//    //   $(this).addClass('#pointer') //remove class to stop animation
+
+//     });
+//   });
+
+function playCompass(){
+  //We'll use this to change the CSS Variable values from JS
+  const style = document.documentElement.style;
+  var angelValue =  document.getElementById("angle").value;
+
+  //alert(angelValue);
+  style.setProperty('--deg', angelValue+'deg');
+
+}
+
+
--- source/js.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.Compass').click(function() {
-      $(this).addClass('.Compass45') //remove class to stop animation
-    });
-  });
-
-
